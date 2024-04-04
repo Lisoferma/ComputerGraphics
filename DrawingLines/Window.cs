756cd5aa3e70b34109bfa449c7e7a693be0f2428
Lisoferma <pilipeiko.sasha@gmail.com>
2024-04-04 20:25:09 +0900

    Big refactoring

--- DrawingLines/Window.cs
+++ DrawingLines/Window.cs
@@ -13,20 +13,22 @@ internal class Window : GameWindow
     private const int CANVAS_WIDTH = 64;
     private const int CANVAS_HEIGHT = 64;
 
-    public int StartX { get; private set; }
+    private readonly Color COLOR_BG_1 = Color.FromArgb(31, 37, 47);
+    private readonly Color COLOR_BG_2 = Color.FromArgb(40, 52, 62);
+    private readonly Color COLOR_DDA = Color.FromArgb(0, 200, 220);
+    private readonly Color COLOR_BRESENHAM = Color.FromArgb(230, 127, 0);
 
-    public int StartY { get; private set; }
-
-    public int EndX { get; private set; }
-
-    public int EndY { get; private set; }
-
-    private float _frameTime = 0.0f;
+    private double _frameTime = 0.0f;
     private int _fps = 0;
 
     private Canvas _canvas;
     private Color[,] _image1;
     private Color[,] _image2;
+
+    private int StartX = -1;
+    private int StartY = -1;
+    private int EndX = -1;
+    private int EndY = -1;
 
     private bool IsDDA = true;
 
@@ -40,16 +42,11 @@ internal class Window : GameWindow
     {
         GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-        StartX = -1;
-        StartY = -1;
-        EndX = -1;
-        EndY = -1;
-
         _image1 = new Color[CANVAS_WIDTH, CANVAS_HEIGHT];
-        CreateGridImage(_image1);
+        CreateGridImage(_image1, COLOR_BG_1, COLOR_BG_2);
 
         _image2 = new Color[CANVAS_WIDTH, CANVAS_HEIGHT];
-        CreateGridImage(_image2);
+        CreateGridImage(_image2, COLOR_BG_1, COLOR_BG_2);
 
         _canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT, ClientSize.X, ClientSize.Y);
         _canvas.SetImage(_image1);
@@ -71,45 +68,7 @@ internal class Window : GameWindow
 
     protected override void OnUpdateFrame(FrameEventArgs e)
     {
-        _frameTime += (float)e.Time;
-        _fps++;
-
-        if (_frameTime >= 1.0f)
-        {
-            if (IsDDA)
-                Title = $"{TITLE} | DDA | {_fps} fps";
-            else
-                Title = $"{TITLE} | Bresenham | {_fps} fps";
-
-            _frameTime = 0.0f;
-            _fps = 0;
-        }
-
-        if (KeyboardState.IsKeyDown(Keys.Escape))
-            Close();
-
-        if (KeyboardState.IsKeyDown(Keys.Backspace))
-        {
-            CreateGridImage(_image2);
-            CreateGridImage(_image1);
-            _canvas.SetImage(_image1);
-        }
-
-        if (KeyboardState.IsKeyPressed(Keys.Space))
-        {
-            if (IsDDA)
-            {
-                _canvas.SetImage(_image2);
-                IsDDA = false;
-            }
-            else
-            {
-                _canvas.SetImage(_image1);
-                IsDDA = true;
-            }
-            
-        }
-
+        UpdateTitle(e.Time);
         base.OnUpdateFrame(e);
     }
 
@@ -130,55 +89,50 @@ internal class Window : GameWindow
     }
 
 
-    private static void CreateGridImage(Color[,] image)
+    protected override void OnKeyDown(KeyboardKeyEventArgs e)
     {
-        int rows = image.GetLength(0);
-        int cols = image.GetLength(1);
+        if (KeyboardState.IsKeyPressed(Keys.Escape))
+            Close();
 
-        bool setColor = false;
+        if (KeyboardState.IsKeyPressed(Keys.Backspace))
+            ClearCanvas();
 
-        for (int i = 0; i < rows; ++i)
-        {
-            for (int j = 0; j < cols; ++j)
-            {
-                if (setColor)
-                    image[i, j] = Color.Gray;
-                else
-                    image[i, j] = Color.DarkGray;
+        if (KeyboardState.IsKeyPressed(Keys.Space))
+            SwitchDisplayedAlgorithm();
 
-                setColor = setColor == false;
-            }
-
-            setColor = setColor == false;
-        }
+        base.OnKeyDown(e);
     }
 
 
     protected override void OnMouseDown(MouseButtonEventArgs e)
     {
+        if (e.Button == MouseButton.Left)
+            SetLineCoord();
+
         base.OnMouseDown(e);
+    }
 
-        if (e.Button != MouseButton.Left)
-            return;
 
+    private void SetLineCoord()
+    {
         (int canvasX, int canvasY) = _canvas.GetCoord(MouseState.X, MouseState.Y);
 
-        if (SetLineCoord(canvasX, canvasY))
+        if (SetBlankLineCoord(canvasX, canvasY))
         {
-            Line.DrawLineDDA(StartX, StartY, EndX, EndY, _image1, Color.Blue);
-            Line.DrawLineBresenham(StartX, StartY, EndX, EndY, _image2, Color.Orange);
-            
+            Line.DrawLineDDA(StartX, StartY, EndX, EndY, _image1, COLOR_DDA);
+            Line.DrawLineBresenham(StartX, StartY, EndX, EndY, _image2, COLOR_BRESENHAM);
+
             if (IsDDA)
                 _canvas.SetImage(_image1);
             else
                 _canvas.SetImage(_image2);
 
-            ClearLineCoor();
+            ClearLineCoord();
         }
     }
 
 
-    private bool SetLineCoord(int x, int y)
+    private bool SetBlankLineCoord(int x, int y)
     {
         if (StartX == -1)
         {
@@ -195,11 +149,76 @@ internal class Window : GameWindow
     }
 
 
-    private void ClearLineCoor()
+    private void ClearLineCoord()
     {
         StartX = -1;
         StartY = -1;
         EndX = -1;
         EndY = -1;
+    }
+
+
+    private void SwitchDisplayedAlgorithm()
+    {
+        if (IsDDA)
+        {
+            _canvas.SetImage(_image2);
+            IsDDA = false;
+        }
+        else
+        {
+            _canvas.SetImage(_image1);
+            IsDDA = true;
+        }
+    }
+
+
+    private void ClearCanvas()
+    {
+        CreateGridImage(_image1, COLOR_BG_1, COLOR_BG_2);
+        CreateGridImage(_image2, COLOR_BG_1, COLOR_BG_2);
+        _canvas.SetImage(_image1);
+    }
+
+
+    private void UpdateTitle(double time)
+    {
+        _frameTime += time;
+        _fps++;
+
+        if (_frameTime >= 1.0f)
+        {
+            if (IsDDA)
+                Title = $"{TITLE} | DDA | {_fps} fps";
+            else
+                Title = $"{TITLE} | Bresenham | {_fps} fps";
+
+            _frameTime = 0.0f;
+            _fps = 0;
+        }
+    }
+
+
+    private static void CreateGridImage(Color[,] image, Color color1, Color color2)
+    {
+        int rows = image.GetLength(0);
+        int cols = image.GetLength(1);
+
+        bool setColor = false;
+
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                if (setColor)
+                    image[i, j] = color1;
+                else
+                    image[i, j] = color2;
+
+                setColor = setColor == false;
+            }
+
+            setColor = setColor == false;
+        }
     }
 }
